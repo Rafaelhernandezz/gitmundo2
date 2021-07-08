9105a300ab3299a851503266571feb83ae6d3329
091810677 rafael <rafahh531@gmail.com>
2021-07-08 18:31:19 -0500

    primer commits

---
+++ contacto.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Contacto</title>
+
+	<link rel="stylesheet" href="css/bootstrap.min2.css">
+    <!-- style css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Responsive-->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- fevicon -->
+    <link rel="icon" href="images/fevicon.png" type="image/gif" />
+    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+</head>
+<body>
+
+	 <div class="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                        <div class="full">
+                            <div class="center-desk">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                        <div class="menu-area">
+                            <div class="limit-box">
+                                <nav class="main-menu">
+                                    <ul class="menu-area-main">
+                                        <li class="active"> <a href="index.php">Inicio</a> </li>
+                                       <li> <a href="contacto.php">Contactame</a> </li>
+                                      
+                                       
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+	 <footer>
+  <div class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+          <div class="JJcoach">
+            <h3>Contactame <strong class="coa"> </strong></h3>
+            </div>
+        
+          </div>
+          
+        
+      </div>
+      <div class="row pdn-top-30">
+        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12">
+          <ul class="location_icon">
+              <li> <img src="icon/location.png"> <span>Pantepec<br>Chiapas</span></li>
+              
+              
+            </ul>
+        </div>
+        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12">
+          <ul class="location_icon">
+              <li> <img src="icon/tellephone.png"><span>+529981875043</span></li>
+            </ul>
+        </div>
+        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12">
+          <ul class="location_icon">
+              <li> <img src="icon/email.png"><span>rafahh531@gmail.com</span></li>
+            </ul>
+        </div>
+
+        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12">
+          <ul class="location_icon">
+              <li> <img src="icon/facebook.png"><span>rafahernandez</span></li>
+            </ul>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <div class="copyright">
+    <div class="container">
+      <p>Copyright 2021 Rafael Hernandez</p>
+    </div>
+  </div>
+</footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" />
+    <link rel="stylesheet" href="css/all.min.css" />
+  
+    <link rel="stylesheet" href="css/templatemo-style.css" />
+
+
+
+
+    <link rel="stylesheet" href="css/bootstrap.min2.css">
+    <!-- style css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Responsive-->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- fevicon -->
+    <link rel="icon" href="images/fevicon.png" type="image/gif" />
+    <!-- Scrollbar Custom CSS -->
+    
+
+
+
+    <title>PAGINA WEB RAFAEL</title>
+    
+
+  </head>
+  <body>
+  	 <div class="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                        <div class="full">
+                            <div class="center-desk">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                        <div class="menu-area">
+                            <div class="limit-box">
+                                <nav class="main-menu">
+                                    <ul class="menu-area-main">
+                                        <li class="active"> <a href="index.php">Inicio</a> </li>
+                                       <li> <a href="contacto.php">Contactame</a> </li>
+                                      
+                                       
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    <!-- Welcome Section -->
+    <section id="tmWelcome" class="parallax-window" data-parallax="scroll" data-image-src="img/mini-profile-bg-01.jpg">
+      <div class="container-fluid tm-brand-container-outer">
+
+        <div class="row">
+          <div class="col-12">
+            <!-- Logo Area -->
+            <!-- Black transparent bg -->
+            <div class="ml-auto mr-0 tm-bg-black-transparent text-white tm-brand-container-inner">
+              <div class="tm-brand-container text-center">
+                <p class="tm-brand-description mb-0">Desarrollo web Integral 9A</p>
+                
+              </div>
+            </div>
+
+            <div class="ml-auto mr-0 tm-bg-black-transparent text-white tm-brand-container-inner">
+                <h2 class="tm-welcome-title">Descripcion del Proyecto General</h2>
+                <p class="pb-0">
+                  Como proyecto general para la materia de desarrollo web se desarrollara un sistema web 
+                  donnde se pueda simular una votacion de la institucion, poder llevar a cabo votaciones de diferentes temas. 
+                  escoger presidente estudiantil, gefes de grupo etc.</p>
+              </div>
+
+
+          </div>
+        </div>
+      </div>
+
+     
+    </section>
+    <!-- Contact section -->
+    <div class="container-fluid">
+        <!-- row -->
+        <div class="row">
+          <footer class="col-12">
+            <p class="text-center tm-copyright-text">
+            Copyright 2021 Rafael Hernandez 
+            
+           </p>
+          </footer>
+        </div>
+      </div>
+    <!-- section -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/parallax.min.js"></script>
+    
+  </body>
+</html>
